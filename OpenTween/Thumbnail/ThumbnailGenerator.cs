685baa44949fe0cf3860d4fed029cde01479befb
spx <spx268@gmail.com>
2012-10-19 10:11:42 +0900

    gyazoのサムネイル表示に対応

--- OpenTween/Thumbnail/ThumbnailGenerator.cs
+++ OpenTween/Thumbnail/ThumbnailGenerator.cs
@@ -169,6 +169,7 @@ namespace OpenTween.Thumbnail
                 new ThumbnailService("FourSquare", Foursquare_GetUrl, Foursquare_CreateImage),
                 new ThumbnailService("TINAMI", Tinami_GetUrl, Tinami_CreateImage),
                 new ThumbnailService("Twimg", Twimg_GetUrl, Twimg_CreateImage),
+                new ThumbnailService("gyazo", gyazo_GetUrl, gyazo_CreateImage),
                 new ThumbnailService("TwitrPix", TwitrPix_GetUrl, TwitrPix_CreateImage),
                 new ThumbnailService("Pckles", Pckles_GetUrl, Pckles_CreateImage),
                 new ThumbnailService("via.me", ViaMe_GetUrl, ViaMe_CreateImage),
@@ -2853,6 +2854,58 @@ namespace OpenTween.Thumbnail
         }
 
 #endregion
+
+        #region "gyazo"
+        /// <summary>
+        /// URL解析部で呼び出されるサムネイル画像URL作成デリゲート
+        /// </summary>
+        /// <param name="args">class GetUrlArgs
+        ///                                 args.url        URL文字列
+        ///                                 args.imglist    解析成功した際にこのリストに元URL、サムネイルURLの形で作成するKeyValuePair
+        /// </param>
+        /// <returns>成功した場合True,失敗の場合False</returns>
+        /// <remarks>args.imglistには呼び出しもとで使用しているimglistをそのまま渡すこと</remarks>
+
+        private bool gyazo_GetUrl(GetUrlArgs args)
+        {
+            var mc = Regex.Match(string.IsNullOrEmpty(args.extended) ? args.url : args.extended,
+                                          @"^http://gyazo\.com/(\w+)(?:\.png)?$", RegexOptions.IgnoreCase);
+            if (mc.Success)
+            {
+                args.imglist.Add(new KeyValuePair<string, string>(args.url, mc.Result("http://gyazo.com/${1}.png")));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// BackgroundWorkerから呼び出されるサムネイル画像作成デリゲート
+        /// </summary>
+        /// <param name="args">class CreateImageArgs
+        ///                                 KeyValuePair<string, string> url                  元URLとサムネイルURLのKeyValuePair
+        ///                                 List<KeyValuePair<string, Image>> pics         元URLとサムネイル画像のKeyValuePair
+        ///                                 List<KeyValuePair<string, string>> tooltiptext 元URLとツールチップテキストのKeyValuePair
+        ///                                 string errmsg                                        取得に失敗した際のエラーメッセージ
+        /// </param>
+        /// <returns>サムネイル画像作成に成功した場合はTrue,失敗した場合はFalse
+        /// なお失敗した場合はargs.errmsgにエラーを表す文字列がセットされる</returns>
+        /// <remarks></remarks>
+
+        private bool gyazo_CreateImage(CreateImageArgs args)
+        {
+            var img = (new HttpVarious()).GetImage(args.url.Value, args.url.Key, 10000, out args.errmsg);
+            if (img == null)
+            {
+                return false;
+            }
+            args.pics.Add(new KeyValuePair<string, Image>(args.url.Key, img));
+            args.tooltipText.Add(new KeyValuePair<string, string>(args.url.Key, ""));
+            return true;
+        }
+        #endregion
 
         #region TwitrPix
 
