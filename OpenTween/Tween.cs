25a3507b741b61c574815a38403d3ef771de6a48
spx <spx268@gmail.com>
2013-03-28 18:31:16 +0900

    URIのエスケープ方法を調整

--- OpenTween/Tween.cs
+++ OpenTween/Tween.cs
@@ -2454,7 +2454,7 @@ namespace OpenTween
             //Google検索（試験実装）
             if (StatusText.Text.StartsWith("Google:", StringComparison.OrdinalIgnoreCase) && StatusText.Text.Trim().Length > 7)
             {
-                string tmp = string.Format(Properties.Resources.SearchItem2Url, Uri.EscapeDataString(StatusText.Text.Substring(7)));
+                string tmp = string.Format(Properties.Resources.SearchItem2Url, Uri.EscapeUriString(StatusText.Text.Substring(7)));
                 OpenUriAsync(tmp);
             }
 
@@ -10368,7 +10368,7 @@ namespace OpenTween
                     return;
                 }
 
-                string tmp = string.Format(url, WebUtility.HtmlDecode(_selText));
+                string tmp = string.Format(url, Uri.EscapeUriString(_selText));
                 OpenUriAsync(tmp);
             }
         }
@@ -13319,7 +13319,7 @@ namespace OpenTween
 
         private void OpenThumbnailPicture(ThumbnailInfo thumbnail)
         {
-            this.OpenUriAsync(thumbnail.ImageUrl);
+            this.OpenUriAsync(Uri.EscapeUriString(thumbnail.ImageUrl));
         }
 
         private void TwitterApiStatusToolStripMenuItem_Click(object sender, EventArgs e)
