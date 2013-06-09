a7b1941986a722accf6dfb79a53cd4b92009aea5
spx <spx268@gmail.com>
2013-06-10 07:33:50 +0900

    複数選択でのコピー（ステータスURL）時に改行されない問題を修正

--- OpenTween/Tween.cs
+++ OpenTween/Tween.cs
@@ -7024,6 +7024,7 @@ namespace OpenTween
             {
                 var post = _statuses[_curTab.Text, idx];
                 sb.Append(MyCommon.GetStatusUrl(post));
+                sb.Append(Environment.NewLine);
             }
             if (sb.Length > 0)
             {
